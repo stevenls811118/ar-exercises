d9702594d7c1014e7980e0ef147c00476ae6d666
Yifan Shi <stevenls1118@msn.com>
2023-03-26 22:40:47 -0600

    exercise_6 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,6 +11,10 @@ store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: fals
 store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 store6 = Store.create(name: "Yaletown", annual_revenue: 430000 , mens_apparel: true, womens_apparel: true)
 
+@store4 = Store.find_by(id: 4)
+@store5 = Store.find_by(id: 5)
+@store6 = Store.find_by(id: 6)
+
 @mens_stores = Store.where("mens_apparel = true")
 
 @mens_stores.each do |i|
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,25 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :stores
+end
+
+# puts "#{@store4.inspect}"
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Steven", last_name: "Shi", hourly_rate: 105)
+@store4.employees.create(first_name: "Jay", last_name: "Zhu", hourly_rate: 70)
+@store5.employees.create(first_name: "Rolin", last_name: "Lee", hourly_rate: 55)
+@store6.employees.create(first_name: "Scott", last_name: "Liang", hourly_rate: 58)
+@store1.employees.create(first_name: "Alex", last_name: "Wang", hourly_rate: 49)
+@store2.employees.create(first_name: "Denny", last_name: "Ma", hourly_rate: 55)
+@store4.employees.create(first_name: "Tony", last_name: "Scott", hourly_rate: 62)
+@store5.employees.create(first_name: "Andrea", last_name: "Jin", hourly_rate: 95)
+@store6.employees.create(first_name: "Michael", last_name: "Shi", hourly_rate: 87)
+@store1.employees.create(first_name: "Penny", last_name: "Owen", hourly_rate: 59)
+@store2.employees.create(first_name: "Jordan", last_name: "Star", hourly_rate: 62)
