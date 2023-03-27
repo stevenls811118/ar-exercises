08264ab880c17ace14800f4fd1ad0f5cf684fd2f
Yifan Shi <stevenls1118@msn.com>
2023-03-27 00:01:38 -0600

    exercise_7 complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,37 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  validates :store_id, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :must_carry_at_least_one_apparel,
+
+  def must_carry_at_least_one_apparel
+    if mens_apparel == nil || womens_apparel == nil
+      errors.add(:apparel, "apparel can not be nil")
+    end
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:apparel, "Must carry at least one apparel")
+    end
+  end
+end
+
+puts "Please type in a store name"
+input_name = gets.chomp
+
+input_store = Store.create(name: input_name)
+
+# puts input_store.errors.messages
+
+input_store.errors.messages.each do |key, value|
+  puts "#{key}: #{value}"
+end
+
